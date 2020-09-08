38be669b0c4f5c004d449ec054b8d4ab1c32fcbd
JoseOgam <jotienoochieng@gmail.com>
2020-09-08 17:27:06 -0400

    components added

--- src/components/header.js
+++ src/components/header.js
@@ -4,24 +4,42 @@ import Contact from './contact'
 // import logo from './logo.svg';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import './styles/header-styles.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function Header () {
   return (
     <Router>
       <div className='Header'>
-        <header className='Header-header'>
-          {/* <img src={logo} className="Header-logo" alt="logo" /> */}
-          <div>
-            <ul>
-              <li>
-                <Link to='/'>Home</Link>
+        <nav className='navbar navbar-expand-lg navbar-primary bg-warning'>
+          <a className='navbar-brand' href=''>
+            Marketer
+          </a>
+          <button
+            className='navbar-toggler'
+            type='button'
+            data-toggle='collapse'
+            data-target='#navbarNav'
+            aria-controls='navbarNav'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
+          >
+            <span className='navbar-toggler-icon'></span>
+          </button>
+          <div className='collapse navbar-collapse my-2 my-lg-0' id='navbarNav'>
+            <ul className='navbar-nav'>
+              <li className='nav-item active'>
+                <Link className='nav-link' to='/'>
+                  Home
+                </Link>
               </li>
-              <li>
-                <Link to='/contact'>Contact Us</Link>
+              <li className='nav-item'>
+                <Link className='nav-link' to='/contact'>
+                  Contact
+                </Link>
               </li>
             </ul>
           </div>
-        </header>
+        </nav>
       </div>
       <Route path='/' exact component={Home} />
       <Route path='/contact' component={Contact} />
