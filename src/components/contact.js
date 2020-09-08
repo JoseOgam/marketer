5e4988a777c151f50923ab8ea5f498388b6834ec
JoseOgam <jotienoochieng@gmail.com>
2020-09-08 15:49:56 -0400

    components added

---
+++ src/components/contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+      <h3>Talk with us</h3>
+    </div>
+  )
+}
+
+export default Contact
--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,31 @@
 import React from 'react'
+import Home from './home'
+import Contact from './contact'
 // import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import './styles/header-styles.css'
 
 function Header () {
   return (
-    <div className='Header'>
-      <header className='Header-header'>
-        {/* <img src={logo} className="Header-logo" alt="logo" /> */}
-      </header>
-    </div>
+    <Router>
+      <div className='Header'>
+        <header className='Header-header'>
+          {/* <img src={logo} className="Header-logo" alt="logo" /> */}
+          <div>
+            <ul>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+              <li>
+                <Link to='/contact'>Contact Us</Link>
+              </li>
+            </ul>
+          </div>
+        </header>
+      </div>
+      <Route path='/' exact component={Home} />
+      <Route path='/contact' component={Contact} />
+    </Router>
   )
 }
 
