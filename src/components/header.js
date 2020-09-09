3c95bce887d2e44f4baab5b8c955ce1fd0080627
JoseOgam <jotienoochieng@gmail.com>
2020-09-09 12:44:13 -0400

    responsive Navbar designing and implementation completed

--- src/components/header.js
+++ src/components/header.js
@@ -1,50 +1,74 @@
-import React from 'react'
-import Home from './home'
-import Contact from './contact'
+import React from "react";
+import Home from "./home";
+import Contact from "./contact";
 // import logo from './logo.svg';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
-import './styles/header-styles.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./styles/header-styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function Header () {
+function Header() {
   return (
     <Router>
-      <div className='Header'>
-        <nav className='navbar navbar-expand-lg navbar-primary bg-warning'>
-          <a className='navbar-brand' href=''>
-            Marketer
-          </a>
-          <button
-            className='navbar-toggler'
-            type='button'
-            data-toggle='collapse'
-            data-target='#navbarNav'
-            aria-controls='navbarNav'
-            aria-expanded='false'
-            aria-label='Toggle navigation'
-          >
-            <span className='navbar-toggler-icon'></span>
-          </button>
-          <div className='collapse navbar-collapse my-2 my-lg-0' id='navbarNav'>
-            <ul className='navbar-nav'>
-              <li className='nav-item active'>
-                <Link className='nav-link' to='/'>
-                  Home
-                </Link>
-              </li>
-              <li className='nav-item'>
-                <Link className='nav-link' to='/contact'>
-                  Contact
-                </Link>
-              </li>
-            </ul>
+      <div>
+        <nav className="navbar navbar-expand-lg py-3 navbar-dark bg-dark shadow-sm">
+          <div className="container">
+            <Link to="#" className="navbar-brand">
+              <img
+                src="https://res.cloudinary.com/mhmd/image/upload/v1557368579/logo_iqjuay.png"
+                width="45"
+                alt=""
+                className="d-inline-block align-middle mr-2"
+              />
+
+              <span className="text-uppercase font-weight-bold">Company</span>
+            </Link>
+
+            <button
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+              className="navbar-toggler"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div
+              id="navbarSupportedContent"
+              className="collapse navbar-collapse"
+            >
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item active">
+                  <Link to="/" className="nav-link">
+                    Home <span className="sr-only">(current)</span>
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="#" className="nav-link">
+                    About
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="#" className="nav-link">
+                    Services
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/contact" className="nav-link">
+                    Contact
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
         </nav>
       </div>
-      <Route path='/' exact component={Home} />
-      <Route path='/contact' component={Contact} />
+      <Route path="/" exact component={Home} />
+      <Route path="/contact" component={Contact} />
     </Router>
-  )
+  );
 }
 
-export default Header
+export default Header;
