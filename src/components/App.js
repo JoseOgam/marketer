5be6caab4a5b74e980cb5fb8f63197b44d18f382
JoseOgam <jotienoochieng@gmail.com>
2020-09-20 12:15:57 -0400

    farm images added

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import HomePage from "./home/HomePage";
 import ContactPage from "./contact/ContactPage";
 import About from "./about/AboutPage";
