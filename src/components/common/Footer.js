f5c9529501c726a3a827b5e044d4d9b5789a5eae
JoseOgam <jotienoochieng@gmail.com>
2020-09-23 13:07:34 -0400

    added hooks to contacts component

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -128,7 +128,7 @@ const Footer = () => {
 
         <div className="footer-copyright text-center py-3">
           © 2020 Copyright:
-          <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+          <a href="#!">bolofarm</a>
         </div>
       </footer>
     </div>
--- src/components/contact/ContactPage.js
+++ src/components/contact/ContactPage.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/contact.css";
 
 const ContactPage = () => {
+  var [contacts, setContact] = useState([]);
+  var [fullname, setFullname] = useState("");
+  var [email, setEmail] = useState("");
+  var [number, setNumber] = useState("");
+  var [subject, setSubject] = useState("");
+  var [message, setMessage] = useState("");
+
+  var handleContacts = (e) => {
+    e.preventDefault();
+    setContact([...contacts, [fullname, email, number, subject, message]]);
+  };
   return (
     <div className="contact-css">
       <div className="container col-md-6">
         <div className="row">
           <div className="col-md-12">
-            <div className="contact-form">
+            <form onSubmit={handleContacts} className="contact-form">
               <div className="form-group">
                 <div className="col-sm-10">
                   <input
@@ -16,6 +27,8 @@ const ContactPage = () => {
                     id="fname"
                     placeholder=" FullName"
                     name="fullname"
+                    value={fullname}
+                    onChange={(e) => setFullname(e.target.value)}
                   />
                 </div>
               </div>
@@ -28,6 +41,8 @@ const ContactPage = () => {
                     id="email"
                     placeholder="Email"
                     name="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                   />
                 </div>
               </div>
@@ -40,6 +55,8 @@ const ContactPage = () => {
                     id="number"
                     placeholder="phone"
                     name="number"
+                    value={number}
+                    onChange={(e) => setNumber(e.target.value)}
                   />
                 </div>
               </div>
@@ -52,6 +69,8 @@ const ContactPage = () => {
                     id="subject"
                     placeholder="subject"
                     name="subject"
+                    value={subject}
+                    onChange={(e) => setSubject(e.target.value)}
                   />
                 </div>
               </div>
@@ -62,6 +81,8 @@ const ContactPage = () => {
                     rows="5"
                     id="comment"
                     name="message"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
                   ></textarea>
                 </div>
               </div>
@@ -72,7 +93,7 @@ const ContactPage = () => {
                   </button>
                 </div>
               </div>
-            </div>
+            </form>
           </div>
         </div>
       </div>
