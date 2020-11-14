f216f047fa7fd14f219918f9e6e770b26965405c
JoseOgam <jotienoochieng@gmail.com>
2020-11-14 10:53:14 -0500

    implement service page and about page

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -1,9 +1,23 @@
 import React from "react";
+import "./About.css"
 
 const About = () => {
   return (
-    <div>
+    <div className="About container col-md-4">
       <h3>About page</h3>
+      <p>
+        We aim to offer our customers a variety of the latest [PRODUCTS CATEGORY NAME]. 
+        We’ve come a long way, so we know exactly which direction to take when supplying 
+        you with high quality yet budget-friendly products.
+        We offer all of this while providing excellent customer service and friendly support.
+
+        We always keep an eye on the latest trends in [PRODUCTS CATEGORY NAME] and put our 
+        customers’ wishes first. That is why we have satisfied customers all over the world,
+         and are thrilled to be a part of the [PRODUCTS CATEGORY NAME] industry.
+
+        The interests of our customers are always top priority for us, so we hope you will 
+        enjoy our products as much as we enjoy making them available to you.
+    </p>
     </div>
   );
 };
--- src/components/services/servicesPage.js
+++ src/components/services/servicesPage.js
@@ -1,9 +1,54 @@
 import React from "react";
+import "./Services.css"
 
 const Services = () => {
   return (
     <div>
-      <h2>services page</h2>
+     
+      <div className='container-fluid mx-auto mt-5 mb-5 col-12 Services'>
+    <div className="hd">Why People Believe in Us</div>
+    <p><small className="text-muted">Always render more and better service than <br />is expected of you, no matter what your ask may be.</small></p>
+    <div className="row cards">
+        <div className="card col-md-3 col-12">
+            <div className="card-content">
+              <div className="card-body">
+                <img className="img" src="https://i.imgur.com/S7FJza5.png" alt="##" />
+                    <div className="shadow"></div>
+                    <div className="card-title"> We're Free </div>
+                    <div className="card-subtitle">
+                        <p> <small className="text-muted">We spent thousands of hours creating on algorithm that does this for you in seconds. We collect a small fee from the professional after they meet your</small> </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="card col-md-3 col-12 ml-2">
+            <div className="card-content">
+              <div className="card-body">
+                <img className="img" src="https://i.imgur.com/xUWJuHB.png" alt="##" />
+                    <div className="card-title"> We're Unbiased </div>
+                    <div className="card-subtitle">
+                        <p> <small className="text-muted"> We don't accept ads from anyone. We use actual data to match you who the best person for each job </small> </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="card col-md-3 col-12 ml-2">
+            <div className="card-content">
+              <div className="card-body">
+                <img className="img rck" src="https://i.imgur.com/rG3CGn3.png" alt="##" />
+                    <div className="card-title"> We Guide you </div>
+                    <div className="card-subtitle">
+                        <p> <small className="text-muted">Buying or selling a home is often the largest transaction anyone does in their life. we guide you through the process so that you can be confident in reaching your dream outcome.</small> </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div className="ft">
+        <p className="chk"><small className="text-muted">Still not sure?</small></p>
+        <div className="btn btn-primary">Read All Testimonials</div>
+    </div>
+</div>
     </div>
   );
 };
